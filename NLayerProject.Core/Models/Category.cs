2363ac8ba4d1bf5ef608dd7fc256e801cb7c37ed
Serkan <sekoylmz14@gmail.com>
2020-09-17 17:03:13 +0300

    Add project files.

---
+++ NLayerProject.Core/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace NLayerProject.Core.Models
+{
+   internal class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+        public int categoryId { get; set; }
+
+        public string categoryName { get; set; }
+
+        public bool isDeleted { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
---
+++ NLayerProject.Core/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLayerProject.Core.Models
+{
+    class Product
+    {
+        public int productId { get; set; }
+
+        public string productName { get; set; }
+
+        public int stock { get; set; }
+
+        public decimal price { get; set; }
+
+        public int categoryId { get; set; }
+
+        public bool isDeleted { get; set; }
+
+        public string innerBarcode { get; set; }
+
+        public virtual Category Category { get; set; }
+
+    }
+}
